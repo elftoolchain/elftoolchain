b482acc2e18bc29f82e745ca27dd5c5e81e5da8e
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2009-11-28 13:35:25 +0000

    Punt on cross-building support (for now).

--- mk/elftoolchain.os.mk
+++ mk/elftoolchain.os.mk
@@ -7,18 +7,7 @@
 # Determine the host operating system flavor.
 OS_HOST != uname -s
 
-# Determine the target operating system that we are building for.
-# We rely on the make(1) builtin variable 'unix' being set
-# correctly in cross-build environments (see <sys.mk>).
-.if ${unix:MFreeBSD*}
-OS_TARGET=freebsd
-.elif ${unix:MNetBSD*}
-OS_TARGET=netbsd
-.else
-.error Unknown target operating system.
-.endif
-
 # Bring in OS-specific Makefiles, if they exist
-.if exists(${.CURDIR}/os.${OS_TARGET}.mk)
-.include "${.CURDIR}/os.${OS_TARGET}.mk"
+.if exists(${.CURDIR}/os.${OS_HOST}.mk)
+.include "${.CURDIR}/os.${OS_HOST}.mk"
 .endif
