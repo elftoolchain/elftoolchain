ac58737dc537c365856893fbb8374d8980b3fe98
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2009-12-13 14:19:58 +0000

    Adjust the CFLAGS variable automagically if LDADD references -lelf.

--- mk/elftoolchain.lib.mk
+++ mk/elftoolchain.lib.mk
@@ -10,6 +10,13 @@
 
 .include <bsd.lib.mk>
 
+.if defined(LDADD)
+_LDADD_LIBELF=${LDADD:M-lelf}
+.if !empty(_LDADD_LIBELF)
+CFLAGS+=	-I${TOP}/libelf
+.endif
+.endif
+
 # Keep the .SUFFIXES line after the include of bsd.lib.mk
 .SUFFIXES:	.m4 .c
 .m4.c:
