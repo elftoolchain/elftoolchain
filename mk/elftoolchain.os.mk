04e68f1948e3b6c690cccc0bf828d92622f9cf15
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2009-12-14 02:10:07 +0000

    Permit project-wide OS-specific build options to be defined in `${TOP}/mk/os.${OS}.mk`.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@557 95820547-d848-0410-985e-9ae8fe0fa350

--- mk/elftoolchain.os.mk
+++ mk/elftoolchain.os.mk
@@ -8,6 +8,11 @@
 OS_HOST != uname -s
 
 # Bring in OS-specific Makefiles, if they exist
+.if exists(${TOP}/mk/os.${OS_HOST}.mk)
+.include "${TOP}/mk/os.${OS_HOST}.mk"
+.endif
+
+# Bring in per-subproject OS-specific Makefiles, if they exist
 .if exists(${.CURDIR}/os.${OS_HOST}.mk)
 .include "${.CURDIR}/os.${OS_HOST}.mk"
 .endif
