59a03a30f7fce4c62a214b06ee974bbea12fc9da
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2009-12-15 14:09:24 +0000

    Add a ruleset for building programs.

---
+++ mk/elftoolchain.prog.mk
@@ -0,0 +1,38 @@
+#
+# Rules for building programs.
+#
+# $Id$
+
+.if !defined(TOP)
+.error	Make variable \"TOP\" has not been defined.
+.endif
+
+.include "${TOP}/mk/elftoolchain.os.mk"
+
+LIBDWARF?=	${TOP}/libdwarf
+LIBELF?=	${TOP}/libelf
+LIBELFTC?=	${TOP}/libelftc
+
+CFLAGS+=	-I. -I${.CURDIR} -I${TOP}/common
+
+.if defined(LDADD)
+_LDADD_LIBDWARF=${LDADD:M-ldwarf}
+.if !empty(_LDADD_LIBDWARF)
+CFLAGS+= -I${TOP}/libdwarf
+LDFLAGS+= -L${TOP}/libdwarf
+.endif
+
+_LDADD_LIBELF=${LDADD:M-lelf}
+.if !empty(_LDADD_LIBELF)
+CFLAGS+= -I${TOP}/libelf
+LDFLAGS+= -L${TOP}/libelf
+.endif
+
+_LDADD_LIBELFTC=${LDADD:M-lelftc}
+.if !empty(_LDADD_LIBELFTC)
+CFLAGS+= -I${TOP}/libelftc
+LDFLAGS+= -L${TOP}/libelftc
+.endif
+.endif
+
+.include <bsd.prog.mk>
