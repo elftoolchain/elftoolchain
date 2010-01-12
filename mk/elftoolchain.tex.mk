f74041db71a9138fb4df192d763e9a00c35a7091
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2010-01-12 17:56:04 +0000

    * Add an '''index''' target that invokes `latex` and `makeindex` as needed to build an upto-date index.  * Automatically invoke `latex` again if the log of the typesetting run had undefined references.  * Also clean the temporary files generated during index generation.

--- mk/elftoolchain.tex.mk
+++ mk/elftoolchain.tex.mk
@@ -12,6 +12,22 @@
 
 all:	${DOC}.pdf
 
+# Build an index.
+#
+# First, we need to remove the existing ".ind" file and run `latex` once
+# to generate it afresh.  This generates the appropriate ".idx" files used
+# by `makeindex`.
+# Next, `makeindex` is used to create the ".ind" file.
+# Then another set of `latex` runs serves to typeset the index.
+index:	.PHONY
+	rm -f ${DOC}.ind
+	${PDFLATEX} ${DOC}.tex
+	${MAKEINDEX} ${DOC}.idx
+	${PDFLATEX} ${DOC}.tex
+	@if grep 'Rerun to get' ${DOC}.log > /dev/null; then \
+		${PDFLATEX} ${DOC}.tex; \
+	fi
+
 # Recognize additional suffixes.
 .SUFFIXES:	.mp .eps .tex .pdf
 
@@ -27,11 +43,16 @@ ${f:R}.eps: ${.CURDIR}/${f}
 CLEANFILES+=	${f:R}.eps ${f:R}.log ${f:R}.pdf ${f:R}.mpx
 .endfor
 
-${DOC}.pdf:	${SRCS} ${IMAGES_MP:S/.mp$/.pdf/g}
+${DOC}.pdf:	${SRCS} ${IMAGES_MP:S/.mp$/.pdf/g} ${DOC}.ind
 	${PDFLATEX} ${DOC}.tex || (rm ${.TARGET}; exit 1)
-	${PDFLATEX} ${DOC}.tex
+	@if grep 'undefined references' ${DOC}.log > /dev/null; then \
+		${PDFLATEX} ${DOC}.tex; \
+	fi
+	@if grep 'Rerun to get' ${DOC}.log > /dev/null; then \
+		${PDFLATEX} ${DOC}.tex; \
+	fi
 
-.for f in aux log out pdf toc
+.for f in aux log out pdf toc ind idx ilg
 CLEANFILES+=	${DOC}.${f}
 .endfor
 
