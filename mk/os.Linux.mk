649000bb26f28b540bdca480219beaf08c2f4feb
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2010-01-13 02:28:50 +0000

    Bring in definitions for building LaTeX documents on Ubuntu GNU/Linux.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@671 95820547-d848-0410-985e-9ae8fe0fa350

--- mk/os.Linux.mk
+++ mk/os.Linux.mk
@@ -5,7 +5,14 @@
 
 MKLINT=		no
 MKPIC=		no
+MKTEX=		yes
 
 OBJECT_FORMAT=	ELF	# work around a bug in the pmake package
 
 YFLAGS+=	-d		# force bison to write y.tab.h
+
+EPSTOPDF=	/usr/bin/epstopdf
+MAKEINDEX=	/usr/bin/makeindex
+MPOST=		/usr/bin/mpost
+MPOSTTEX=	/usr/bin/latex
+PDFLATEX=	/usr/bin/pdflatex
