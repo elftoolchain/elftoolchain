d1d8e61b52b6e82b63a2e95e92ba626d477e8cf6
kaiwang27 <kaiwang27@95820547-d848-0410-985e-9ae8fe0fa350>
2010-03-13 03:53:30 +0000

    * Added _dwarf_frame_fde_add_inst which transforms FDE frame opcodes into bytes stream. * Implemented producer API dwarf_fde_cfa_offset and dwarf_add_fde_inst.

--- libdwarf/libdwarf_frame.c
+++ libdwarf/libdwarf_frame.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2009 Kai Wang
+ * Copyright (c) 2009, 2010 Kai Wang
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -1131,4 +1131,99 @@ _dwarf_frame_init(Dwarf_Debug dbg, Dwarf_Error *error)
 	dbg->dbg_internal_reg_table = rt;
 
 	return (DWARF_E_NONE);
+}
+
+#define	_FDE_INST_INIT_SIZE	128
+
+int
+_dwarf_frame_fde_add_inst(Dwarf_P_Fde fde, Dwarf_Small op, Dwarf_Unsigned val1,
+    Dwarf_Unsigned val2, Dwarf_Error *error)
+{
+	Dwarf_P_Debug dbg;
+	uint8_t high2, low6;
+	int ret;
+
+#define	ds	fde
+#define	ds_data	fde_inst
+#define	ds_cap	fde_instcap
+#define	ds_size	fde_instlen
+
+	assert(fde != NULL && fde->fde_dbg != NULL);
+	dbg = fde->fde_dbg;
+
+	if (fde->fde_inst == NULL) {
+		fde->fde_instcap = _FDE_INST_INIT_SIZE;
+		fde->fde_instlen = 0;
+		if ((fde->fde_inst = malloc(fde->fde_instcap)) == NULL) {
+			DWARF_SET_ERROR(error, DWARF_E_MEMORY);
+			return (DWARF_E_MEMORY);
+		}
+	}
+	assert(fde->fde_instcap != 0);
+
+	RCHECK(WRITE_VALUE(op, 1));
+	if (op == DW_CFA_nop)
+		return (DWARF_E_NONE);
+
+	high2 = op & 0xc0;
+	low6 = op & 0x3f;
+
+	if (high2 > 0) {
+		switch (high2) {
+		case DW_CFA_advance_loc:
+		case DW_CFA_restore:
+			break;
+		case DW_CFA_offset:
+			RCHECK(WRITE_ULEB128(val1));
+			break;
+		default:
+			DWARF_SET_ERROR(error, DWARF_E_INVALID_FRAME);
+			return (DWARF_E_INVALID_FRAME);
+		}
+		return (DWARF_E_NONE);
+	}
+
+	switch (low6) {
+	case DW_CFA_set_loc:
+		RCHECK(WRITE_VALUE(val1, dbg->dbg_pointer_size));
+		break;
+	case DW_CFA_advance_loc1:
+		RCHECK(WRITE_VALUE(val1, 1));
+		break;
+	case DW_CFA_advance_loc2:
+		RCHECK(WRITE_VALUE(val1, 2));
+		break;
+	case DW_CFA_advance_loc4:
+		RCHECK(WRITE_VALUE(val1, 4));
+		break;
+	case DW_CFA_offset_extended:
+	case DW_CFA_def_cfa:
+	case DW_CFA_register:
+		RCHECK(WRITE_ULEB128(val1));
+		RCHECK(WRITE_ULEB128(val2));
+		break;
+	case DW_CFA_restore_extended:
+	case DW_CFA_undefined:
+	case DW_CFA_same_value:
+	case DW_CFA_def_cfa_register:
+	case DW_CFA_def_cfa_offset:
+		RCHECK(WRITE_ULEB128(val1));
+		break;
+	case DW_CFA_remember_state:
+	case DW_CFA_restore_state:
+		break;
+	default:
+		DWARF_SET_ERROR(error, DWARF_E_INVALID_FRAME);
+		return (DWARF_E_INVALID_FRAME);
+	}
+	
+	return (DWARF_E_NONE);
+
+gen_fail:
+	return (ret);
+
+#undef	ds
+#undef	ds_data
+#undef	ds_cap
+#undef	ds_size	
 }
