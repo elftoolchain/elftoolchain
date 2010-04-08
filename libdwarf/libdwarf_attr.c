69bf10af2628df39704edee826d5e11ea14d12f6
kaiwang27 <kaiwang27@95820547-d848-0410-985e-9ae8fe0fa350>
2010-04-08 17:11:16 +0000

    Removed duplicate "All rights reserved." clause.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@867 95820547-d848-0410-985e-9ae8fe0fa350

--- libdwarf/libdwarf_attr.c
+++ libdwarf/libdwarf_attr.c
@@ -1,7 +1,6 @@
 /*-
- * Copyright (c) 2009, 2010 Kai Wang
- * All rights reserved.
  * Copyright (c) 2007 John Birrell (jb@freebsd.org)
+ * Copyright (c) 2009, 2010 Kai Wang
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
