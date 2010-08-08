c86d176dda569e37f5cba7f4e1f7d7082aa8e6ea
kaiwang27 <kaiwang27@95820547-d848-0410-985e-9ae8fe0fa350>
2010-08-08 15:10:15 +0000

    Implement option -c: Dump arsym for ar(1) archives.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@1061 95820547-d848-0410-985e-9ae8fe0fa350

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -27,6 +27,7 @@
 #include <sys/cdefs.h>
 #include <sys/param.h>
 #include <sys/queue.h>
+#include <ar.h>
 #include <ctype.h>
 #include <dwarf.h>
 #include <err.h>
@@ -5529,18 +5530,75 @@ ac_dump_ar(struct readelf *re, int fd)
 static void
 dump_ar(struct readelf *re, int fd)
 {
+	Elf_Arsym *arsym;
 	Elf_Arhdr *arhdr;
 	Elf_Cmd cmd;
+	Elf *e;
+	size_t sz;
+	off_t off;
+	int i;
 
 	re->ar = re->elf;
+
+	if (re->options & RE_C) {
+		if ((arsym = elf_getarsym(re->ar, &sz)) == NULL) {
+			warnx("elf_getarsym() failed: %s", elf_errmsg(-1));
+			goto process_members;
+		}
+		printf("Index of archive %s: (%ju entries)\n", re->filename,
+		    (uintmax_t) sz - 1);
+		off = 0;
+		for (i = 0; (size_t) i < sz; i++) {
+			if (arsym[i].as_name == NULL)
+				break;
+			if (arsym[i].as_off != off) {
+				off = arsym[i].as_off;
+				if (elf_rand(re->ar, off) != off) {
+					warnx("elf_rand() failed: %s",
+					    elf_errmsg(-1));
+					continue;
+				}
+				if ((e = elf_begin(fd, ELF_C_READ, re->ar)) ==
+				    NULL) {
+					warnx("elf_begin() failed: %s",
+					    elf_errmsg(-1));
+					continue;
+				}
+				if ((arhdr = elf_getarhdr(e)) == NULL) {
+					warnx("elf_getarhdr() failed: %s",
+					    elf_errmsg(-1));
+					elf_end(e);
+					continue;
+				}
+				printf("Binary %s(%s) contains:\n",
+				    re->filename, arhdr->ar_name);
+			}
+			printf("\t%s\n", arsym[i].as_name);
+		}
+		if (elf_rand(re->ar, SARMAG) != SARMAG) {
+			warnx("elf_rand() failed: %s", elf_errmsg(-1));
+			return;
+		}
+	}
+
+process_members:
+
+	if ((re->options & ~RE_C) == 0)
+		return;
+
 	cmd = ELF_C_READ;
 	while ((re->elf = elf_begin(fd, cmd, re->ar)) != NULL) {
 		if ((arhdr = elf_getarhdr(re->elf)) == NULL) {
 			warnx("elf_getarhdr() failed: %s", elf_errmsg(-1));
-			continue;
+			goto next_member;
 		}
+		if (strcmp(arhdr->ar_name, "/") == 0 ||
+		    strcmp(arhdr->ar_name, "//") == 0)
+			goto next_member;
 		printf("\nFile: %s(%s)\n", re->filename, arhdr->ar_name);
 		dump_elf(re);
+
+	next_member:
 		cmd = elf_next(re->elf);
 		elf_end(re->elf);
 	}
