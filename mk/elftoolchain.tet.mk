f9b5bc12a99b893fb0f7bb74e784b3b463562419
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2010-08-16 13:09:47 +0000

    Only generate the scaffolding required by TET if the make variable `GENERATE_TEST_SCAFFOLDING` is set.
    Suggested by:	kaiwang27

--- mk/elftoolchain.tet.mk
+++ mk/elftoolchain.tet.mk
@@ -20,38 +20,42 @@ TET_OBJS=	${TET_LIBS}/tcm.o
 
 CFLAGS+=	-I${TET_ROOT}/inc/tet3 -I${TS_ROOT}/common
 
+# Bring in test-suite specific definitions, if any.
+.if exists(${.CURDIR}/../Makefile.tset)
+.include "${.CURDIR}/../Makefile.tset"
+.endif
+
 .if defined(TS_SRCS)
 PROG=		tc_${.CURDIR:T:R}
 
 _C_SRCS=	${TS_SRCS:M*.c}
 _M4_SRCS=	${TS_SRCS:M*.m4}
 
-_TC_SRC=	${.OBJDIR}/tc.c				# Test driver.
-
-SRCS=		${_TC_SRC} ${_C_SRCS} ${_M4_SRCS}	# See <bsd.prog.mk>.
-CLEANFILES+=	${_M4_SRCS:S/.m4$/.c/g}  ${.OBJDIR}/tc.c ${TS_DATA}
+SRCS=		${_C_SRCS} ${_M4_SRCS}	# See <bsd.prog.mk>.
+CLEANFILES+=	${_M4_SRCS:S/.m4$/.c/g} ${TS_DATA}
 
 ${PROG}:	${TS_DATA} 
 
 NO_MAN?=	1
+
+.if defined(GENERATE_TEST_SCAFFOLDING)
+_TC_SRC=	${.OBJDIR}/tc.c				# Test driver.
+
+SRCS+=		${_TC_SRC}
+CLEANFILES+=	${_TC_SRC}
 
 # Generate the driver file "tc.c" from the objects comprising the test case.
 _TS_OBJS=	${_C_SRCS:S/.c$/.o/g} ${_M4_SRCS:S/.m4$/.o/g}
 _MUNGE_TS=	${TS_ROOT}/bin/munge-ts
 ${_TC_SRC}:	${_TS_OBJS}
 	${_MUNGE_TS} -o ${.TARGET} ${.ALLSRC}
-
+.endif
 .endif
 
 # M4->C translation.
 M4FLAGS+=	-I${TS_ROOT}/common
 .m4.c:
 	m4 ${M4FLAGS} ${.IMPSRC} > ${.TARGET}
-
-# Bring in test-suite specific definitions, if any.
-.if exists(${.CURDIR}/../Makefile.tset)
-.include "${.CURDIR}/../Makefile.tset"
-.endif
 
 LDADD+=		${TET_OBJS} -L${TET_LIBS} -lapi
 CLEANFILES+=	tet_xres
