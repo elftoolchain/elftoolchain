c00dac1bd914dfd699794b0b6e90d78351f51a68
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2010-08-22 07:18:30 +0000

    Move TET version information to a separate `.mk` file.

---
+++ mk/elftoolchain.tetvars.mk
@@ -0,0 +1,12 @@
+#
+# Configuration information for TET.
+#
+# $Id$
+#
+
+# Set TET_ROOT and version.
+TET_VERSION?=		3.8
+TET_ROOT?=		${TOP}/test/tet/tet${TET_VERSION}
+
+TET_DOWNLOAD_URL=	\
+	http://tetworks.opengroup.org/downloads/38/software/Sources/${TET_VERSION}/tet${TET_VERSION}-src.tar.gz
