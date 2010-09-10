aef54980fd9c329d04fa2ef762604d7f109ab3f1
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2010-09-10 12:28:11 +0000

    Add `PT_GNU_RELRO` (a GNU extension).
    This segment type denotes a segment that is to be read-only after
    relocations are performed.
    
    Ticket: #265
    
    
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@1179 95820547-d848-0410-985e-9ae8fe0fa350

--- common/elfdefinitions.h
+++ common/elfdefinitions.h
@@ -638,6 +638,8 @@ _ELF_DEFINE_PT(PT_GNU_EH_FRAME,     0x6474E550UL,		\
 	"GCC generated .eh_frame_hdr segment ")			\
 _ELF_DEFINE_PT(PT_GNU_STACK,	    0x6474E551UL,		\
 	"Stack flags")						\
+_ELF_DEFINE_PT(PT_GNU_RELRO,	    0x6474E552UL,		\
+	"Segment becomes read-only after relocation")		\
 _ELF_DEFINE_PT(PT_HIOS,             0x6FFFFFFFUL,		\
 	"end of OS-specific range")				\
 _ELF_DEFINE_PT(PT_LOPROC,           0x70000000UL,		\
