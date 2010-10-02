c9668feb52e451f073c3bfde2b5c28080080f010
kaiwang27 <kaiwang27@95820547-d848-0410-985e-9ae8fe0fa350>
2010-10-02 17:49:40 +0000

    More verbose output (violating file name and line number) if DWARF debugging information is available.

--- findtextrel/findtextrel.c
+++ findtextrel/findtextrel.c
@@ -94,11 +94,19 @@ static void
 report_textrel(const char *fn, Elf *e, Dwarf_Debug dbg, uintmax_t off,
     int *textrel)
 {
+	Dwarf_Die die;
+	Dwarf_Line *lbuf;
+	Dwarf_Error de;
+	Dwarf_Half tag;
+	Dwarf_Unsigned lopc, hipc, lineno, plineno;
+	Dwarf_Signed lcount;
+	Dwarf_Addr lineaddr, plineaddr;
 	Elf_Scn *scn;
 	Elf_Data *d;
 	GElf_Shdr sh;
 	const char *name;
-	int elferr;
+	char *file, *pfile;
+	int elferr, found, i, ret;
 
 	if (!*textrel) {
 		printf("%s: ELF object contains text relocation records:\n",
@@ -106,39 +114,115 @@ report_textrel(const char *fn, Elf *e, Dwarf_Debug dbg, uintmax_t off,
 		*textrel = 1;
 	}
 
-	if (dbg == NULL) {
-		printf("%s:   off: %#jx", fn, off);
-		scn = NULL;
-		while ((scn = elf_nextscn(e, scn)) != NULL) {
-			if (gelf_getshdr(scn, &sh) == NULL) {
-				warnx("%s: gelf_getshdr() failed: %s", fn,
-				    elf_errmsg(-1));
-				continue;
-			}
-			if (sh.sh_type != SHT_DYNSYM &&
-			    sh.sh_type != SHT_SYMTAB)
-				continue;
-			(void) elf_errno();
-			if ((d = elf_getdata(scn, NULL)) == NULL) {
-				elferr = elf_errno();
-				if (elferr != 0)
-					warnx("%s: elf_getdata() failed: %s",
-					    fn, elf_errmsg(-1));
-				continue;
-			}
-			if (d->d_size <= 0)
-				continue;
-			if ((name = find_symbol(fn, e, d, &sh, off)) != NULL) {
-				printf(", func: %s", name);
-				break;
-			}
+	printf("%s:   off: %#jx", fn, off);
+
+	scn = NULL;
+	while ((scn = elf_nextscn(e, scn)) != NULL) {
+		if (gelf_getshdr(scn, &sh) == NULL) {
+			warnx("%s: gelf_getshdr() failed: %s", fn,
+			    elf_errmsg(-1));
+			continue;
 		}
-		elferr = elf_errno();
-		if (elferr != 0)
-			warnx("%s: elf_nextscn() failed: %s", fn,
-			    elf_errmsg(elferr));
+		if (sh.sh_type != SHT_DYNSYM &&
+		    sh.sh_type != SHT_SYMTAB)
+			continue;
+		(void) elf_errno();
+		if ((d = elf_getdata(scn, NULL)) == NULL) {
+			elferr = elf_errno();
+			if (elferr != 0)
+				warnx("%s: elf_getdata() failed: %s",
+				    fn, elf_errmsg(-1));
+			continue;
+		}
+		if (d->d_size <= 0)
+			continue;
+		if ((name = find_symbol(fn, e, d, &sh, off)) != NULL) {
+			printf(", func: %s", name);
+			break;
+		}
+	}
+	elferr = elf_errno();
+	if (elferr != 0)
+		warnx("%s: elf_nextscn() failed: %s", fn,
+		    elf_errmsg(elferr));
+
+	if (dbg == NULL)
+		goto done;
+
+	/*
+	 * More verbose output if debugging information is available.
+	 */
+
+	while ((ret = dwarf_next_cu_header(dbg, NULL, NULL, NULL, NULL, NULL,
+	    &de)) == DW_DLV_OK) {
+		die = NULL;
+		while (dwarf_siblingof(dbg, die, &die, &de) == DW_DLV_OK) {
+			if (dwarf_tag(die, &tag, &de) != DW_DLV_OK)
+				goto out;
+			/* XXX: What about DW_TAG_partial_unit? */
+			if (tag == DW_TAG_compile_unit)
+				break;
+		}
+		if (die == NULL) {
+			/* Could not find DW_TAG_compile_unit DIE. */
+			goto out;
+		}
+		if (!dwarf_attrval_unsigned(die, DW_AT_low_pc, &lopc, &de) &&
+		    !dwarf_attrval_unsigned(die, DW_AT_high_pc, &hipc, &de)) {
+			/*
+			 * Check if the address falls into the PC range of
+			 * this CU.
+			 */
+			if (off < lopc || off >= hipc)
+				continue;
+		} else
+			continue;
+
+		if (dwarf_srclines(die, &lbuf, &lcount, &de) != DW_DLV_OK)
+			continue;
+
+		found = 0;
+		plineaddr = ~0ULL;
+		plineno = 0;
+		pfile = NULL;
+		for (i = 0; i < lcount; i++) {
+			if (dwarf_lineaddr(lbuf[i], &lineaddr, &de))
+				continue;
+			if (dwarf_lineno(lbuf[i], &lineno, &de))
+				continue;
+			if (dwarf_linesrc(lbuf[i], &file, &de))
+				continue;
+			if (off == lineaddr) {
+				found = 1;
+				goto out;
+			} else if (off < lineaddr && off > plineaddr) {
+				lineno = plineno;
+				file = pfile;
+				found = 1;
+				goto out;
+			}
+			plineaddr = lineaddr;
+			plineno = lineno;
+			pfile = file;
+		}
 	}
 
+out:
+	if (found)
+		printf(", file: %s, line: %ju", file, lineno);
+
+	/*
+	 * Reset internal CU pointer, so we will start from the first CU
+	 * next round.
+	 */
+	while (ret != DW_DLV_NO_ENTRY) {
+		if (ret == DW_DLV_ERROR)
+			break;
+		ret = dwarf_next_cu_header(dbg, NULL, NULL, NULL, NULL, NULL,
+		    &de);
+	}
+
+done:
 	putchar('\n');
 }
 
@@ -250,7 +334,6 @@ find_textrel(const char *fn)
 	/* Check if debugging information is available. */
 	if (dwarf_elf_init(e, DW_DLC_READ, NULL, NULL, &dbg, &de))
 		dbg = NULL;
-	dbg = NULL;
 
 	/*
 	 * Search relocation records for possible text relocations.
