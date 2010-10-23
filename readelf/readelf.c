105782f06996bc217acd9c528c478e581a34afce
kaiwang27 <kaiwang27@95820547-d848-0410-985e-9ae8fe0fa350>
2010-10-23 10:27:13 +0000

    Fix timestamp format string.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@1242 95820547-d848-0410-985e-9ae8fe0fa350

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -2426,7 +2426,7 @@ timestamp(time_t ti)
 	struct tm *t;
 
 	t = gmtime(&ti);
-	snprintf(ts, sizeof(ts), "%04d-%02d-%02dT%02d:%02d:%2d",
+	snprintf(ts, sizeof(ts), "%04d-%02d-%02dT%02d:%02d:%02d",
 	    t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour,
 	    t->tm_min, t->tm_sec);
 
