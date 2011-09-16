b3b0cdbb955ae32717641c736eda78765b181834
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2011-09-16 13:30:35 +0000

    Style fixes for `nm`'s usage message.
    Ticket:	#324
    
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@1900 95820547-d848-0410-985e-9ae8fe0fa350

--- nm/nm.c
+++ nm/nm.c
@@ -2034,50 +2034,50 @@ usage(int exitcode)
 {
 
 	printf("Usage: %s [options] file ...\
-\n  Display symbolic information in file.\
-\n  Options : \
-\n    -A, --print-file-name     Write the full pathname or library name of an\
-\n                              object on each line.\
-\n    -a, --debug-syms          Display all symbols include debugger-only\
-\n                              symbols.", nm_info.name);
+\n  Display symbolic information in file.\n\
+\n  Options: \
+\n  -A, --print-file-name     Write the full pathname or library name of an\
+\n                            object on each line.\
+\n  -a, --debug-syms          Display all symbols include debugger-only\
+\n                            symbols.", nm_info.name);
 	printf("\
-\n    -B                        Equivalent to specifying \"--format=bsd\".\
-\n    -C, --demangle[=style]    Decode low-level symbol names.\
-\n        --no-demangle         Do not demangle low-level symbol names.\
-\n    -D, --dynamic             Display only dynamic symbols.\
-\n    -e                        Display only global and static symbols.");
+\n  -B                        Equivalent to specifying \"--format=bsd\".\
+\n  -C, --demangle[=style]    Decode low-level symbol names.\
+\n      --no-demangle         Do not demangle low-level symbol names.\
+\n  -D, --dynamic             Display only dynamic symbols.\
+\n  -e                        Display only global and static symbols.");
 	printf("\
-\n    -f                        Produce full output (default).\
-\n        --format=format       Display output in specific format.  Allowed\
-\n                              formats are: \"bsd\", \"posix\" and \"sysv\".\
-\n    -g                        Display only global symbol information.\
-\n    -h, --help                Show this help message.\
-\n    -l, --line-numbers        Display filename and linenumber using\
-\n                              debugging information.\
-\n    -n, --numeric-sort        Sort symbols numerically by value.");
+\n  -f                        Produce full output (default).\
+\n      --format=format       Display output in specific format.  Allowed\
+\n                            formats are: \"bsd\", \"posix\" and \"sysv\".\
+\n  -g                        Display only global symbol information.\
+\n  -h, --help                Show this help message.\
+\n  -l, --line-numbers        Display filename and linenumber using\
+\n                            debugging information.\
+\n  -n, --numeric-sort        Sort symbols numerically by value.");
 	printf("\
-\n    -o                        Write numeric values in octal. Equivalent to\
-\n                              specifying \"-t o\".\
-\n    -p, --no-sort             Do not sort symbols.\
-\n    -P                        Write information in a portable output format.\
-\n                              Equivalent to specifying \"--format=posix\".\
-\n    -r, --reverse-sort        Reverse the order of the sort.\
-\n    -S, --print-size          Print symbol sizes instead values.\
-\n    -s, --print-armap         Include an index of archive members.\
-\n        --size-sort           Sort symbols by size.");
+\n  -o                        Write numeric values in octal. Equivalent to\
+\n                            specifying \"-t o\".\
+\n  -p, --no-sort             Do not sort symbols.\
+\n  -P                        Write information in a portable output format.\
+\n                            Equivalent to specifying \"--format=posix\".\
+\n  -r, --reverse-sort        Reverse the order of the sort.\
+\n  -S, --print-size          Print symbol sizes instead values.\
+\n  -s, --print-armap         Include an index of archive members.\
+\n      --size-sort           Sort symbols by size.");
 	printf("\
-\n    -t, --radix=format        Write each numeric value in the specified\
-\n                              format:\
-\n                                 d   In decimal,\
-\n                                 o   In octal,\
-\n                                 x   In hexadecimal.");
+\n  -t, --radix=format        Write each numeric value in the specified\
+\n                            format:\
+\n                               d   In decimal,\
+\n                               o   In octal,\
+\n                               x   In hexadecimal.");
 	printf("\
-\n    -u, --undefined-only      Display only undefined symbols.\
-\n        --defined-only        Display only defined symbols.\
-\n    -V, --version             Show the version identifier for %s.\
-\n    -v                        Sort output by value.\
-\n    -x                        Write numeric values in hexadecimal.\
-\n                              Equivalent to specifying \"-t x\".",
+\n  -u, --undefined-only      Display only undefined symbols.\
+\n      --defined-only        Display only defined symbols.\
+\n  -V, --version             Show the version identifier for %s.\
+\n  -v                        Sort output by value.\
+\n  -x                        Write numeric values in hexadecimal.\
+\n                            Equivalent to specifying \"-t x\".",
 	    nm_info.name);
 	printf("\n\
 \n  The default options are: output in bsd format, use a decimal radix,\
