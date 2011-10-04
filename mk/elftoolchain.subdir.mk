d0dc8f8cf7655e736f7ff33eea1562b54642f2f6
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2011-10-04 10:59:51 +0000

    Adapt to the fact that FreeBSD spells the `TARGETS` configuration knob differently.
    Ticket:	#290
    
    
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@2011 95820547-d848-0410-985e-9ae8fe0fa350

--- mk/elftoolchain.subdir.mk
+++ mk/elftoolchain.subdir.mk
@@ -4,6 +4,12 @@
 
 # Pass down 'test' as a valid target.
 
+.include "$(TOP)/mk/elftoolchain.os.mk"
+
+.if ${OS_HOST} == FreeBSD
+SUBDIR_TARGETS+=	test
+.else
 TARGETS+=	test
+.endif
 
 .include <bsd.subdir.mk>
