432d6f3b08fedac9787affd6a1b7eff04910b458
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2011-10-31 03:29:24 +0000

    Update a test case to match the behavior of '''ar''' post changeset [2094].
    Reviewed by:	kaiwang27
    Ticket:		#353
    
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@2095 95820547-d848-0410-985e-9ae8fe0fa350

--- test/ar/tc/addself-liba/addself-liba.sh
+++ test/ar/tc/addself-liba/addself-liba.sh
@@ -3,4 +3,6 @@ inittest addself-liba tc/addself-liba
 extshar ${TESTDIR}
 extshar ${RLTDIR}
 runcmd "${AR} cru liba.a liba.a" work true
-rundiff true
+rundiff false
+runcmd "plugin/teraser -c -t addself-liba liba.a" work false
+runcmd "plugin/ardiff -cnlt addself-liba ${RLTDIR}/liba.a liba.a" work false
