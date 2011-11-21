32a21039bd1b87a975e40a9ea2b44f3c501aef7c
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2011-11-21 08:14:19 +0000

    Style tweaks to '''nm''': use a symbolic constant instead of a numeric literal.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@2189 95820547-d848-0410-985e-9ae8fe0fa350

--- nm/nm.c
+++ nm/nm.c
@@ -426,7 +426,7 @@ parse_demangle_option(const char *opt)
 {
 
 	if (opt == NULL)
-		return (0);
+		return (ELFTC_DEM_UNKNOWN);
 	else if (!strncasecmp(opt, "gnu-v2", 6))
 		return (ELFTC_DEM_GNU2);
 	else if (!strncasecmp(opt, "gnu-v3", 6))
