977b3e30cff1c59d85df32dc4716f0b6656624a0
kaiwang27 <kaiwang27@95820547-d848-0410-985e-9ae8fe0fa350>
2011-11-28 01:29:02 +0000

    Create exp module. Exp module handles the expressions found in the linker scripts.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@2233 95820547-d848-0410-985e-9ae8fe0fa350

---
+++ ld/ld_exp.c
@@ -0,0 +1,315 @@
+/*-
+ * Copyright (c) 2011 Kai Wang
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include "ld.h"
+#include "ld_exp.h"
+
+ELFTC_VCSID("$Id$");
+
+/*
+ * Support routines for ldscript expression.
+ */
+
+static int64_t _assignment(struct ld *ld, struct ld_exp *le);
+static int64_t _func_addr(struct ld *ld, struct ld_exp *le);
+static int64_t _func_align(struct ld *ld, struct ld_exp *le);
+static int64_t _func_alignof(struct ld *ld, struct ld_exp *le);
+static int64_t _func_data_segment_align(struct ld *ld, struct ld_exp *le);
+static int64_t _func_data_segment_end(struct ld *ld, struct ld_exp *le);
+static int64_t _func_data_segment_relro_end(struct ld *ld, struct ld_exp *le);
+static int64_t _func_defined(struct ld *ld, struct ld_exp *le);
+static int64_t _func_length(struct ld *ld, struct ld_exp *le);
+static int64_t _func_loadaddr(struct ld *ld, struct ld_exp *le);
+static int64_t _func_max(struct ld *ld, struct ld_exp *le);
+static int64_t _func_min(struct ld *ld, struct ld_exp *le);
+static int64_t _func_next(struct ld *ld, struct ld_exp *le);
+static int64_t _func_origin(struct ld *ld, struct ld_exp *le);
+static int64_t _func_segment_start(struct ld *ld, struct ld_exp *le);
+static int64_t _func_sizeof(struct ld *ld, struct ld_exp *le);
+static int64_t _func_sizeof_headers(struct ld *ld, struct ld_exp *le);
+static int64_t _variable(struct ld *ld, const char *name);
+
+#define	_EXP_EVAL(x) ld_exp_eval(ld, (x))
+
+int64_t
+ld_exp_eval(struct ld* ld, struct ld_exp *le)
+{
+
+	switch (le->le_op) {
+	case LEOP_ABS:
+		return (abs(_EXP_EVAL(le)));
+	case LEOP_ADD:
+		return (_EXP_EVAL(le->le_e1) + _EXP_EVAL(le->le_e2));
+	case LEOP_ADDR:
+		return (_func_addr(ld, le));
+	case LEOP_ALIGN:
+		return (_func_align(ld, le));
+	case LEOP_ALIGNOF:
+		return (_func_alignof(ld, le));
+	case LEOP_AND:
+		return (_EXP_EVAL(le->le_e1) & _EXP_EVAL(le->le_e2));
+	case LEOP_ASSIGN:
+		return (_assignment(ld, le));
+	case LEOP_BLOCK:
+		return (_func_align(ld, le));
+	case LEOP_CONSTANT:
+		return (le->le_val);
+	case LEOP_DIV:
+		return (_EXP_EVAL(le->le_e1) / _EXP_EVAL(le->le_e2));
+	case LEOP_DSA:
+		return (_func_data_segment_align(ld, le));
+	case LEOP_DSE:
+		return (_func_data_segment_end(ld, le));
+	case LEOP_DSRE:
+		return (_func_data_segment_relro_end(ld, le));
+	case LEOP_DEFINED:
+		return (_func_defined(ld, le));
+	case LEOP_EQUAL:
+		return (_EXP_EVAL(le->le_e1) == _EXP_EVAL(le->le_e2));
+	case LEOP_GE:
+		return (_EXP_EVAL(le->le_e1) >= _EXP_EVAL(le->le_e2));
+	case LEOP_GREATER:
+		return (_EXP_EVAL(le->le_e1) > _EXP_EVAL(le->le_e2));
+	case LEOP_LENGTH:
+		return (_func_length(ld, le));
+	case LEOP_LOADADDR:
+		return (_func_loadaddr(ld, le));
+	case LEOP_LOGICAL_AND:
+		return (_EXP_EVAL(le->le_e1) && _EXP_EVAL(le->le_e2));
+	case LEOP_LOGICAL_OR:
+		return (_EXP_EVAL(le->le_e1) || _EXP_EVAL(le->le_e2));
+	case LEOP_LSHIFT:
+		return (_EXP_EVAL(le->le_e1) << _EXP_EVAL(le->le_e2));
+	case LEOP_MAX:
+		return (_func_max(ld, le));
+	case LEOP_MIN:
+		return (_func_min(ld, le));
+	case LEOP_MINUS:
+		return (-(_EXP_EVAL(le->le_e1)));
+	case LEOP_MOD:
+		return (_EXP_EVAL(le->le_e1) % _EXP_EVAL(le->le_e2));
+	case LEOP_MUL:
+		return (_EXP_EVAL(le->le_e1) * _EXP_EVAL(le->le_e2));
+	case LEOP_NE:
+		return (_EXP_EVAL(le->le_e1) != _EXP_EVAL(le->le_e2));
+	case LEOP_NEGATION:
+		return (~(_EXP_EVAL(le->le_e1)));
+	case LEOP_NEXT:
+		return (_func_next(ld, le));
+	case LEOP_NOT:
+		return (!(_EXP_EVAL(le->le_e1)));
+	case LEOP_OR:
+		return (_EXP_EVAL(le->le_e1) | _EXP_EVAL(le->le_e2));
+	case LEOP_ORIGIN:
+		return (_func_origin(ld, le));
+	case LEOP_RSHIFT:
+		return (_EXP_EVAL(le->le_e1) >> _EXP_EVAL(le->le_e2));
+	case LEOP_SEGMENT_START:
+		return (_func_segment_start(ld, le));
+	case LEOP_SIZEOF:
+		return (_func_sizeof(ld, le));
+	case LEOP_SIZEOF_HEADERS:
+		return (_func_sizeof_headers(ld, le));
+	case LEOP_SUBSTRACT:
+		return (_EXP_EVAL(le->le_e1) - _EXP_EVAL(le->le_e2));
+	case LEOP_TRINARY:
+		return (_EXP_EVAL(le->le_e1) ? _EXP_EVAL(le->le_e2) :
+		    _EXP_EVAL(le->le_e3));
+	case LEOP_VAR:
+		return (_variable(ld, le->le_name));
+	default:
+		ld_fatal(ld, "internal: unknown ldscript expression op");
+	}
+
+	return (0);
+}
+
+static int64_t
+_assignment(struct ld *ld, struct ld_exp *le)
+{
+
+	/* TODO */
+	(void) ld; (void) le;
+	return (0);
+}
+
+static int64_t
+_func_addr(struct ld *ld, struct ld_exp *le)
+{
+
+	/* TODO */
+	(void) ld; (void) le;
+	return (0);
+}
+
+static int64_t
+_func_align(struct ld *ld, struct ld_exp *le)
+{
+
+	if (le->le_e2 != NULL)
+		return (roundup(_EXP_EVAL(le->le_e1), _EXP_EVAL(le->le_e2)));
+	else
+		return (roundup(_variable(ld, "."), _EXP_EVAL(le->le_e2)));
+}
+
+static int64_t
+_func_alignof(struct ld *ld, struct ld_exp *le)
+{
+
+	/* TODO */
+	(void) ld; (void) le;
+	return (0);
+}
+
+static int64_t
+_func_data_segment_align(struct ld *ld, struct ld_exp *le)
+{
+
+	/* TODO */
+	(void) ld; (void) le;
+	return (0);
+}
+
+static int64_t
+_func_data_segment_end(struct ld *ld, struct ld_exp *le)
+{
+
+	/* TODO */
+	(void) ld; (void) le;
+	return (0);
+}
+
+static int64_t
+_func_data_segment_relro_end(struct ld *ld, struct ld_exp *le)
+{
+
+	/* TODO */
+	(void) ld; (void) le;
+	return (0);
+}
+
+static int64_t
+_func_defined(struct ld *ld, struct ld_exp *le)
+{
+
+	/* TODO */
+	(void) ld; (void) le;
+	return (0);
+}
+
+static int64_t
+_func_length(struct ld *ld, struct ld_exp *le)
+{
+
+	/* TODO */
+	(void) ld; (void) le;
+	return (0);
+}
+
+static int64_t
+_func_loadaddr(struct ld *ld, struct ld_exp *le)
+{
+
+	/* TODO */
+	(void) ld; (void) le;
+	return (0);
+}
+
+static int64_t
+_func_max(struct ld *ld, struct ld_exp *le)
+{
+	uint64_t val1, val2;
+
+	val1 = _EXP_EVAL(le->le_e1);
+	val2 = _EXP_EVAL(le->le_e2);
+
+	return (val1 > val2 ? val1 : val2);
+}
+
+static int64_t
+_func_min(struct ld *ld, struct ld_exp *le)
+{
+	uint64_t val1, val2;
+
+	val1 = _EXP_EVAL(le->le_e1);
+	val2 = _EXP_EVAL(le->le_e2);
+
+	return (val1 > val2 ? val2 : val1);
+}
+
+static int64_t
+_func_next(struct ld *ld, struct ld_exp *le)
+{
+
+	/* TODO */
+	(void) ld; (void) le;
+	return (0);
+}
+
+static int64_t
+_func_origin(struct ld *ld, struct ld_exp *le)
+{
+
+	/* TODO */
+	(void) ld; (void) le;
+	return (0);
+}
+
+static int64_t
+_func_segment_start(struct ld *ld, struct ld_exp *le)
+{
+
+	/* TODO */
+	(void) ld; (void) le;
+	return (0);
+}
+
+static int64_t
+_func_sizeof(struct ld *ld, struct ld_exp *le)
+{
+
+	/* TODO */
+	(void) ld; (void) le;
+	return (0);
+}
+
+static int64_t
+_func_sizeof_headers(struct ld *ld, struct ld_exp *le)
+{
+
+	/* TODO */
+	(void) ld; (void) le;
+	return (0);
+}
+
+static int64_t
+_variable(struct ld *ld, const char *name)
+{
+
+	/* TODO */
+	(void) ld; (void) name;
+	return (0);
+}
---
+++ ld/ld_exp.h
@@ -0,0 +1,83 @@
+/*-
+ * Copyright (c) 2011 Kai Wang
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id$
+ */
+
+enum ld_exp_op {
+	LEOP_ABS,
+	LEOP_ADD,
+	LEOP_ADDR,
+	LEOP_ALIGN,
+	LEOP_ALIGNOF,
+	LEOP_AND,
+	LEOP_ASSIGN,
+	LEOP_BLOCK,
+	LEOP_CONSTANT,
+	LEOP_DIV,
+	LEOP_DSA,
+	LEOP_DSE,
+	LEOP_DSRE,
+	LEOP_DEFINED,
+	LEOP_EQUAL,
+	LEOP_GE,
+	LEOP_GREATER,
+	LEOP_LENGTH,
+	LEOP_LE,
+	LEOP_LESSER,
+	LEOP_LOADADDR,
+	LEOP_LOGICAL_AND,
+	LEOP_LOGICAL_OR,
+	LEOP_LSHIFT,
+	LEOP_MAX,
+	LEOP_MIN,
+	LEOP_MINUS,
+	LEOP_MOD,
+	LEOP_MUL,
+	LEOP_NE,
+	LEOP_NEGATION,
+	LEOP_NEXT,
+	LEOP_NOT,
+	LEOP_OR,
+	LEOP_ORIGIN,
+	LEOP_RSHIFT,
+	LEOP_SEGMENT_START,
+	LEOP_SIZEOF,
+	LEOP_SIZEOF_HEADERS,
+	LEOP_SUBSTRACT,
+	LEOP_TRINARY,
+	LEOP_VAR
+};
+
+struct ld_exp {
+	enum ld_exp_op le_op;	/* expression operator */
+	struct ld_exp *le_e1;	/* fisrt operand */
+	struct ld_exp *le_e2;	/* second operand */
+	struct ld_exp *le_e3;	/* third operand */
+	char *le_name;		/* symbol/section name */
+	int64_t le_val;		/* constant value */
+};
+
+int64_t		ld_exp_eval(struct ld *ld, struct ld_exp *le);
