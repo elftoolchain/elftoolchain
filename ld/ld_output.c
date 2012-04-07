d643f97336e387b04f55af615cea676d3f74289a
kaiwang27 <kaiwang27@95820547-d848-0410-985e-9ae8fe0fa350>
2012-04-07 19:17:51 +0000

    Specify O_CREAT flag when opening output file.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@2487 95820547-d848-0410-985e-9ae8fe0fa350

--- ld/ld_output.c
+++ ld/ld_output.c
@@ -522,7 +522,7 @@ ld_output_create(struct ld *ld)
 
 	lo = ld->ld_output;
 
-	if ((lo->lo_fd = open(fn, O_WRONLY)) < 0)
+	if ((lo->lo_fd = open(fn, O_WRONLY | O_CREAT)) < 0)
 		ld_fatal_std(ld, "can not create output file: open %s", fn);
 
 	if ((lo->lo_elf = elf_begin(lo->lo_fd, ELF_C_WRITE, NULL)) == NULL)
