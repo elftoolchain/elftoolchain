b10e1c7affc11501ce806379f2c5cb7dbcf56ba1
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2013-01-13 15:36:04 +0000

    Changes to the `isa` utility:  * Use the `-D`, `-E`, and `-Q` options, to specify the mode the    utility is to operate in.  * Expand the description of the command modes supported by the `isa`    utility in the manual page.  * Implement the rest of the command-line parser.  * Add an EXAMPLE section to the manual page.
    Ticket:	#412
    
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@2889 95820547-d848-0410-985e-9ae8fe0fa350

--- isa/isa.c
+++ isa/isa.c
@@ -24,9 +24,13 @@
  * SUCH DAMAGE.
  */
 
+#include <sys/queue.h>
+
 #include <getopt.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include <libelftc.h>
@@ -35,18 +39,60 @@
 
 ELFTC_VCSID("$Id$");
 
+/*
+ * Option handling.
+ */
+
+enum isa_mode {
+	ISA_MODE_DECODE,
+	ISA_MODE_ENCODE,
+	ISA_MODE_QUERY
+};
+
+enum isa_submode {
+	ISA_SUBMODE_GENERATE_TESTS,
+	ISA_SUBMODE_LIST_INSTRUCTIONS
+};
+
+#define	ISA_OPT_DRY_RUN			0x0001
+#define	ISA_OPT_NO_WARNINGS		0x0002
+#define	ISA_OPT_VERBOSE			0x0004
+
+/* Record a option. */
+struct isa_option {
+	const char		*isa_option;
+	SLIST_ENTRY(isa_option)	isa_next;
+};
+
+struct isa_config {
+	unsigned int		isa_flags;
+	enum isa_mode		isa_mode;
+	enum isa_submode	isa_submode;
+	int			isa_ntests;
+	int			isa_seed;
+	const char		*isa_arch;
+	const char		*isa_input;
+	const char		*isa_output;
+	const char		*isa_prefix;
+	SLIST_HEAD(,isa_option) isa_cpus;
+	SLIST_HEAD(,isa_option) isa_specs;
+};
+
 #define	ISA_MAX_LONG_OPTION_LENGTH	64
 
 static struct option isa_long_options[] = {
 	{ "arch",  required_argument, NULL, 'a' },
 	{ "cpu", required_argument, NULL, 'c' },
+	{ "decode", no_argument, NULL, 'D' },
 	{ "dry-run", no_argument, NULL, 'n' },
+	{ "encode", no_argument, NULL, 'E' },
 	{ "help", no_argument, NULL, 'h' },
 	{ "input", required_argument, NULL, 'i' },
 	{ "list-instructions", no_argument, NULL, 'L' },
 	{ "ntests", required_argument, NULL, 'N' },
 	{ "output", required_argument, NULL, 'o' },
 	{ "prefix", required_argument, NULL, 'p' },
+	{ "query", no_argument, NULL, 'Q' },
 	{ "quiet", no_argument, NULL, 'q' },
 	{ "random-seed", required_argument, NULL, 'R' },
 	{ "spec", required_argument, NULL, 's' },
@@ -90,10 +136,20 @@ Supported options for command 'query' are:\n\
 ";
 
 void
-isa_usage(int iserror)
+isa_usage(int iserror, const char *message, ...)
 {
-	(void) fprintf(iserror ? stderr : stdout, isa_usage_message,
-	    ELFTC_GETPROGNAME());
+	FILE *channel;
+	va_list ap;
+
+	channel = iserror ? stderr : stdout;
+
+	if (message) {
+		va_start(ap, message);
+		(void) vfprintf(channel, message, ap);
+		va_end(ap);
+	}
+
+	(void) fprintf(channel, isa_usage_message, ELFTC_GETPROGNAME());
 	exit(iserror != 0);
 }
 
@@ -111,18 +167,39 @@ isa_unimplemented(int option, int option_index, struct option *options_table)
 	errx(1, "ERROR: option %s is unimplemented.", msgbuf);
 }
 
+struct isa_option *
+isa_make_option(const char *arg)
+{
+	struct isa_option *isa_opt;
+
+	if ((isa_opt = malloc(sizeof(*isa_opt))) == NULL)
+		return (NULL);
+	isa_opt->isa_option = optarg;
+
+	return (isa_opt);
+}
+
 int
 main(int argc, char **argv)
 {
 	int option, option_index;
+	struct isa_option *isa_opt;
+	struct isa_config config;
+
+	(void) memset(&config, 0, sizeof(config));
+	config.isa_mode = ISA_MODE_QUERY;
+	config.isa_arch = config.isa_input = config.isa_output =
+	    config.isa_prefix = NULL;
+	SLIST_INIT(&config.isa_cpus);
+	SLIST_INIT(&config.isa_specs);
 
 	for (option_index = -1;
-	     (option = getopt_long(argc, argv, "a:c:hi:no:p:qs:vLN:R:TV",
+	     (option = getopt_long(argc, argv, "a:c:hi:no:p:qs:vDELN:QR:TV",
 		 isa_long_options, &option_index)) != -1;
 	     option_index = -1) {
 		switch (option) {
 		case 'h':
-			isa_usage(0);
+			isa_usage(0, NULL);
 			break;
 		case 'V':
 			(void) printf("%s (%s)\n", ELFTC_GETPROGNAME(),
@@ -130,17 +207,79 @@ main(int argc, char **argv)
 			exit(0);
 			break;
 
-		case 'C': case 'D': case 'E':
-		case 'L': case 'M': case 'm':
-		case 'o': case 'p': case 'R':
-		case 'Z':
-			isa_unimplemented(option, option_index,
-			    isa_long_options);
+		case 'a':
+			config.isa_arch = optarg;
+			break;
+		case 'c':
+			if ((isa_opt = isa_make_option(optarg)) == NULL)
+				goto error;
+			SLIST_INSERT_HEAD(&config.isa_cpus, isa_opt, isa_next);
+			break;
+		case 'i':
+			config.isa_input = optarg;
+			break;
+		case 'n':
+			config.isa_flags |= ISA_OPT_DRY_RUN;
+			break;
+		case 'o':
+			config.isa_output = optarg;
+			break;
+		case 'p':
+			config.isa_prefix = optarg;
+			break;
+		case 'q':
+			config.isa_flags |= ISA_OPT_NO_WARNINGS;
+			break;
+		case 's':
+			if ((isa_opt = isa_make_option(optarg)) == NULL)
+				goto error;
+			SLIST_INSERT_HEAD(&config.isa_specs, isa_opt,
+			    isa_next);
+			break;
+		case 'v':
+			config.isa_flags |= ISA_OPT_VERBOSE;
+			break;
+		case 'D':
+			config.isa_mode = ISA_MODE_DECODE;
+			break;
+		case 'E':
+			config.isa_mode = ISA_MODE_ENCODE;
+			break;
+		case 'L':
+			config.isa_submode = ISA_SUBMODE_LIST_INSTRUCTIONS;
+			break;
+		case 'N':
+			config.isa_ntests = atoi(optarg);
+			break;
+		case 'Q':
+			config.isa_mode = ISA_MODE_QUERY;
+			break;
+		case 'R':
+			config.isa_seed = atoi(optarg);
+			break;
+		case 'T':
+			config.isa_submode = ISA_SUBMODE_GENERATE_TESTS;
 			break;
 		default:
-			isa_usage(1);
+			isa_usage(1, "\n");
 			break;
 		}
 	}
+
+	/*
+	 * Create the canonical list of specification files to
+	 * be processed.
+	 */
+	while (optind < argc) {
+		if ((isa_opt = isa_make_option(argv[optind])) == NULL)
+			goto error;
+		SLIST_INSERT_HEAD(&config.isa_specs, isa_opt,
+		    isa_next);
+	}
+
+	exit(0);
+
+error:
+	err(1, "ERROR: Invocation failed");
 }
 
