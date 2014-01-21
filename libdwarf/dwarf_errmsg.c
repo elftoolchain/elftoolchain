9068917fded1afa684cd7533489c1907cb3f4743
kaiwang27 <kaiwang27@95820547-d848-0410-985e-9ae8fe0fa350>
2014-01-21 20:08:04 +0000

    Fix a warning in libdwarf found by -Wmissing-variable-declarations.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@2975 95820547-d848-0410-985e-9ae8fe0fa350

--- libdwarf/dwarf_errmsg.c
+++ libdwarf/dwarf_errmsg.c
@@ -28,7 +28,7 @@
 
 ELFTC_VCSID("$Id$");
 
-const char *_libdwarf_errors[] = {
+static const char *_libdwarf_errors[] = {
 #define	DEFINE_ERROR(N,S)		[DW_DLE_##N] = S
 	DEFINE_ERROR(NONE, "No Error"),
 	DEFINE_ERROR(ERROR, "An error"),
