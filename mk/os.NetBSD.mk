4dcbc6de5d37751d6ea749e28a5e5c5450a8a940
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2014-03-06 03:29:12 +0000

    Add a NetBSD-specific 'clobber' target that removes '.cat[0-9]' and '.html[0-9]' files generated from manual page sources.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@2986 95820547-d848-0410-985e-9ae8fe0fa350

--- mk/os.NetBSD.mk
+++ mk/os.NetBSD.mk
@@ -11,3 +11,11 @@ MKNOWEB?=	no		# Build literate programs.
 
 # Literate programming utility.
 NOWEB?=		/usr/pkgsrc/bin/noweb
+
+# NetBSD's 'clean' target does not remove 'cat[0-9]' and 'html[0-9]'
+# files generate from manual page sources.  Augment the 'clobber'
+# target to remove these.
+os-specific-clobber: .PHONY
+.for f in cat html
+	rm -f ${MANPAGES:@M@${M:R}.$f${M:E}@}
+.endfor
