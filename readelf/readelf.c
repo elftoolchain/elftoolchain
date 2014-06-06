c4b408231ab9e33dd0735635b2ac17e992f4400c
kaiwang27 <kaiwang27@95820547-d848-0410-985e-9ae8fe0fa350>
2014-06-06 19:36:13 +0000

    Add support for dumping GNU DWARF operator extentions.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@3067 95820547-d848-0410-985e-9ae8fe0fa350

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -274,7 +274,7 @@ static void dump_dwarf_info(struct readelf *re, Dwarf_Bool is_info);
 static void dump_dwarf_macinfo(struct readelf *re);
 static void dump_dwarf_line(struct readelf *re);
 static void dump_dwarf_line_decoded(struct readelf *re);
-static void dump_dwarf_loc(Dwarf_Loc *lr);
+static void dump_dwarf_loc(struct readelf *re, Dwarf_Loc *lr);
 static void dump_dwarf_loclist(struct readelf *re);
 static void dump_dwarf_pubnames(struct readelf *re);
 static void dump_dwarf_ranges(struct readelf *re);
@@ -5799,6 +5799,9 @@ search_loclist_at(struct readelf *re, Dwarf_Die die, Dwarf_Unsigned lowpc)
 			nla->la_at = attr_list[i];
 			nla->la_off = off;
 			nla->la_lowpc = lowpc;
+			nla->la_cu_psize = re->cu_psize;
+			nla->la_cu_osize = re->cu_osize;
+			nla->la_cu_ver = re->cu_ver;
 			TAILQ_INSERT_TAIL(&lalist, nla, la_next);
 		}
 	}
@@ -5820,10 +5823,12 @@ cont_search:
 }
 
 static void
-dump_dwarf_loc(Dwarf_Loc *lr)
+dump_dwarf_loc(struct readelf *re, Dwarf_Loc *lr)
 {
 	const char *op_str;
 	char unk_op[32];
+	uint8_t *b, n;
+	int i;
 
 	if (dwarf_get_OP_name(lr->lr_atom, &op_str) !=
 	    DW_DLV_OK) {
@@ -5927,6 +5932,12 @@ dump_dwarf_loc(Dwarf_Loc *lr)
 	case DW_OP_lt:
 	case DW_OP_ne:
 	case DW_OP_nop:
+	case DW_OP_push_object_address:
+	case DW_OP_form_tls_address:
+	case DW_OP_call_frame_cfa:
+	case DW_OP_stack_value:
+	case DW_OP_GNU_push_tls_address:
+	case DW_OP_GNU_uninit:
 		break;
 
 	case DW_OP_const1u:
@@ -5995,8 +6006,49 @@ dump_dwarf_loc(Dwarf_Loc *lr)
 		break;
 
 	case DW_OP_addr:
+	case DW_OP_GNU_encoded_addr:
 		printf(": %#jx", (uintmax_t)
 		    lr->lr_number);
+		break;
+
+	case DW_OP_GNU_implicit_pointer:
+		printf(": <0x%jx> %jd", (uintmax_t) lr->lr_number,
+		    (intmax_t) lr->lr_number2);
+		break;
+
+	case DW_OP_implicit_value:
+		printf(": %ju byte block:", (uintmax_t) lr->lr_number);
+		b = (uint8_t *)(uintptr_t) lr->lr_number2;
+		for (i = 0; (Dwarf_Unsigned) i < lr->lr_number; i++)
+			printf(" %x", b[i]);
+		break;
+
+	case DW_OP_GNU_entry_value:
+		printf(": (");
+		dump_dwarf_block(re, (uint8_t *)(uintptr_t) lr->lr_number2,
+		    lr->lr_number);
+		putchar(')');
+		break;
+
+	case DW_OP_GNU_const_type:
+		printf(": <0x%jx> ", (uintmax_t) lr->lr_number);
+		b = (uint8_t *)(uintptr_t) lr->lr_number2;
+		n = *b;
+		for (i = 1; (uint8_t) i < n; i++)
+			printf(" %x", b[i]);
+		break;
+
+	case DW_OP_GNU_regval_type:
+		/* TODO: show reg name */
+		printf(": %ju <0x%jx>", (uintmax_t) lr->lr_number,
+		    (uintmax_t) lr->lr_number2);
+		break;
+
+	case DW_OP_GNU_convert:
+	case DW_OP_GNU_deref_type:
+	case DW_OP_GNU_parameter_ref:
+	case DW_OP_GNU_reinterpret:
+		printf(": <0x%jx>", (uintmax_t) lr->lr_number);
 		break;
 
 	default:
@@ -6019,7 +6071,7 @@ dump_dwarf_block(struct readelf *re, uint8_t *b, Dwarf_Unsigned len)
 	}
 
 	for (i = 0; (Dwarf_Half) i < llbuf->ld_cents; i++) {
-		dump_dwarf_loc(&llbuf->ld_s[i]);
+		dump_dwarf_loc(re, &llbuf->ld_s[i]);
 		if (i < llbuf->ld_cents - 1)
 			printf("; ");
 	}
@@ -6128,7 +6180,7 @@ dump_dwarf_loclist(struct readelf *re)
 
 			putchar('(');
 			for (j = 0; (Dwarf_Half) j < llbuf[i]->ld_cents; j++) {
-				dump_dwarf_loc(&llbuf[i]->ld_s[j]);
+				dump_dwarf_loc(re, &llbuf[i]->ld_s[j]);
 				if (j < llbuf[i]->ld_cents - 1)
 					printf("; ");
 			}
