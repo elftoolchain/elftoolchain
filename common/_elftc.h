a7ac33c495eec9492c6ed442a1c97979da8c14bb
kaiwang27 <kaiwang27@95820547-d848-0410-985e-9ae8fe0fa350>
2015-01-05 03:17:06 +0000

    Add missing definition for LIST_FOREACH_SAFE under Linux.
    Ticket:	#477
    
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@3139 95820547-d848-0410-985e-9ae8fe0fa350

--- common/_elftc.h
+++ common/_elftc.h
@@ -76,10 +76,17 @@
  * SUCH DAMAGE.
  */
 
+#ifndef	LIST_FOREACH_SAFE
+#define	LIST_FOREACH_SAFE(var, head, field, tvar)		\
+	for ((var) = LIST_FIRST((head));			\
+	    (var) && ((tvar) = LIST_NEXT((var), field), 1);	\
+	    (var) = (tvar))
+#endif
+
 #ifndef	SLIST_FOREACH_SAFE
-#define	SLIST_FOREACH_SAFE(var, head, field, tvar)			\
-	for ((var) = SLIST_FIRST((head));				\
-	    (var) && ((tvar) = SLIST_NEXT((var), field), 1);		\
+#define	SLIST_FOREACH_SAFE(var, head, field, tvar)		\
+	for ((var) = SLIST_FIRST((head));			\
+	    (var) && ((tvar) = SLIST_NEXT((var), field), 1);	\
 	    (var) = (tvar))
 #endif
 
