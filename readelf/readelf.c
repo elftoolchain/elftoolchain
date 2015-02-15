eb45b1832adf3111797e6a15a3d082e0bd05f47d
emaste <emaste@95820547-d848-0410-985e-9ae8fe0fa350>
2015-02-15 19:15:32 +0000

    readelf: sort Linux core note types in numerical order
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@3152 95820547-d848-0410-985e-9ae8fe0fa350

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -1561,7 +1561,12 @@ note_type_linux_core(unsigned int nt)
 	case 3: return "NT_PRPSINFO (Process information)";
 	case 4: return "NT_TASKSTRUCT (Task structure)";
 	case 6: return "NT_AUXV (Auxiliary vector)";
-	case 0x46E62B7FUL: return "NT_PRXFPREG (Linux user_xfpregs structure)";
+	case 10: return "NT_PSTATUS (Linux process status)";
+	case 12: return "NT_FPREGS (Linux floating point regset)";
+	case 13: return "NT_PSINFO (Linux process information)";
+	case 16: return "NT_LWPSTATUS (Linux lwpstatus_t type)";
+	case 17: return "NT_LWPSINFO (Linux lwpinfo_t type)";
+	case 18: return "NT_WIN32PSTATUS (win32_pstatus structure)";
 	case 0x100: return "NT_PPC_VMX (ppc Altivec registers)";
 	case 0x102: return "NT_PPC_VSX (ppc VSX registers)";
 	case 0x202: return "NT_X86_XSTATE (x86 XSAVE extended state)";
@@ -1572,12 +1577,7 @@ note_type_linux_core(unsigned int nt)
 	case 0x304: return "NT_S390_CTRS (s390 control registers)";
 	case 0x305: return "NT_S390_PREFIX (s390 prefix register)";
 	case 0x400: return "NT_ARM_VFP (arm VFP registers)";
-	case 10: return "NT_PSTATUS (Linux process status)";
-	case 12: return "NT_FPREGS (Linux floating point regset)";
-	case 13: return "NT_PSINFO (Linux process information)";
-	case 16: return "NT_LWPSTATUS (Linux lwpstatus_t type)";
-	case 17: return "NT_LWPSINFO (Linux lwpinfo_t type)";
-	case 18: return "NT_WIN32PSTATUS (win32_pstatus structure)";
+	case 0x46E62B7FUL: return "NT_PRXFPREG (Linux user_xfpregs structure)";
 	default: return (note_type_unknown(nt));
 	}
 }
