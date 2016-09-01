934c7a2382ecf945c4d5df1720abe0f926903f5e
emaste <emaste@95820547-d848-0410-985e-9ae8fe0fa350>
2016-09-01 14:06:57 +0000

    cpp_demangle_gnu3: demangle 'z' as '...', not 'ellipsis'
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@3491 95820547-d848-0410-985e-9ae8fe0fa350

--- libelftc/libelftc_dem_gnu3.c
+++ libelftc/libelftc_dem_gnu3.c
@@ -2572,7 +2572,7 @@ again:
 
 	case 'z':
 		/* ellipsis */
-		if (!cpp_demangle_push_str(ddata, "ellipsis", 8))
+		if (!cpp_demangle_push_str(ddata, "...", 3))
 			goto clean;
 		++ddata->cur;
 		goto rtn;
