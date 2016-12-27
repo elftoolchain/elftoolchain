4cd4b229d83a0efd114bb3619a6a1da5213ac9c0
kaiwang27 <kaiwang27@95820547-d848-0410-985e-9ae8fe0fa350>
2016-12-27 06:19:39 +0000

    Do not try to copy section content from a NULL d_buf when creating uninitialized data COFF section for PE object.
    Ticket:		#540
    Reported by:	emaste
    
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@3508 95820547-d848-0410-985e-9ae8fe0fa350

--- elfcopy/pe.c
+++ elfcopy/pe.c
@@ -210,12 +210,14 @@ create_pe(struct elfcopy *ecp, int ifd, int ofd)
 		}
 		pb->pb_align = 1;
 		pb->pb_off = 0;
-		pb->pb_size = roundup(sh.sh_size, poh.oh_filealign);
-		if ((pb->pb_buf = calloc(1, pb->pb_size)) == NULL) {
-			warn("calloc failed");
-			continue;
+		if (sh.sh_type != SHT_NOBITS) {
+			pb->pb_size = roundup(sh.sh_size, poh.oh_filealign);
+			if ((pb->pb_buf = calloc(1, pb->pb_size)) == NULL) {
+				warn("calloc failed");
+				continue;
+			}
+			memcpy(pb->pb_buf, d->d_buf, sh.sh_size);
 		}
-		memcpy(pb->pb_buf, d->d_buf, sh.sh_size);
 	}
 	elferr = elf_errno();
 	if (elferr != 0)
