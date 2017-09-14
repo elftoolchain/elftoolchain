1927c3b8d42357cf9499753fe4afaff2bedfd5fe
emaste <emaste@95820547-d848-0410-985e-9ae8fe0fa350>
2017-09-14 23:54:59 +0000

    readelf: add NT_ARM_VFP FreeBSD ELF core note type
    Submitted by:	John Baldwin
    Obtained from:	FreeBSD r323588
    
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@3579 95820547-d848-0410-985e-9ae8fe0fa350

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -1138,6 +1138,7 @@ note_type_freebsd_core(unsigned int nt)
 	case 16: return "NT_PROCSTAT_AUXV";
 	case 17: return "NT_PTLWPINFO";
 	case 0x202: return "NT_X86_XSTATE (x86 XSAVE extended state)";
+	case 0x400: return "NT_ARM_VFP (arm VFP registers)";
 	default: return (note_type_unknown(nt));
 	}
 }
