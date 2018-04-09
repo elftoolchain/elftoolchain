836f476b270f49c7e8018a9d6b47829e9146124e
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2018-04-09 19:49:19 +0000

    Permit build knobs specified on the command-line to override configuration defaults.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@3591 95820547-d848-0410-985e-9ae8fe0fa350

--- mk/elftoolchain.components.mk
+++ mk/elftoolchain.components.mk
@@ -5,17 +5,17 @@
 # Knobs to turn parts of the source tree on or off.
 
 # Build the automation tools.
-WITH_BUILD_TOOLS=	no
+WITH_BUILD_TOOLS?=	no
 
 # Build additional tutorial documentation. (Manual page generation is
 # controlled by the 'MKDOC' knob).
-WITH_ADDITIONAL_DOCUMENTATION=yes
+WITH_ADDITIONAL_DOCUMENTATION?=yes
 
 # Build the instruction set analyser.
-WITH_ISA=	no
+WITH_ISA?=	no
 
 # Build PE support.
-WITH_PE=	yes
+WITH_PE?=	yes
 
 # Build test suites.
-WITH_TESTS=	yes
+WITH_TESTS?=	yes
