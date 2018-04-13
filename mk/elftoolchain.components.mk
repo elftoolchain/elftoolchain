b9a7f386df7430beae0973b508f53325b66470a4
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2018-04-13 19:41:07 +0000

    Catch typos in WITH_<KNOB> values early, and fail the build with an informative error message if needed.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@3606 95820547-d848-0410-985e-9ae8fe0fa350

--- mk/elftoolchain.components.mk
+++ mk/elftoolchain.components.mk
@@ -3,6 +3,8 @@
 #
 
 # Knobs to turn parts of the source tree on or off.
+#
+# These knobs should be set to one of "yes" or "no".
 
 # Build the automation tools.
 WITH_BUILD_TOOLS?=	no
@@ -22,7 +24,27 @@ WITH_PE?=	yes
 .if defined(WITH_TESTS) && ${WITH_TESTS} == "yes"
 .error "Only in-tree builds are supported for tests currently [#271]."
 .endif
-WITH_TESTS=no
+WITH_TESTS?=	no
 .else
 WITH_TESTS?=	yes
+.endif
+
+# Fail the build with an informative message if the value of any
+# build knob is not a "yes" or "no".
+.if ${WITH_ADDITIONAL_DOCUMENTATION} != "yes" && \
+    ${WITH_ADDITIONAL_DOCUMENTATION} != "no"
+.error Unrecognized value for WITH_ADDITIONAL_DOCUMENTATION:\
+       "${WITH_ADDITIONAL_DOCUMENTATION}".
+.endif
+.if ${WITH_BUILD_TOOLS} != "yes" && ${WITH_BUILD_TOOLS} != "no"
+.error Unrecognized value for WITH_BUILD_TOOLS: "${WITH_BUILD_TOOLS}".
+.endif
+.if ${WITH_ISA} != "yes" && ${WITH_ISA} != "no"
+.error Unrecognized value for WITH_ISA: "${WITH_ISA}".
+.endif
+.if ${WITH_PE} != "yes" && ${WITH_PE} != "no"
+.error Unrecognized value for WITH_PE: "${WITH_PE}".
+.endif
+.if ${WITH_TESTS} != "yes" && ${WITH_TESTS} != "no"
+.error Unrecognized value for WITH_TESTS: "${WITH_TESTS}".
 .endif
