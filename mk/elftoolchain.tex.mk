dbf6bef02cefbe906d0c2f92dd6843c7934bda34
jkoshy <jkoshy@95820547-d848-0410-985e-9ae8fe0fa350>
2020-11-09 21:06:32 +0000

    Bug fix: remove the generated 'version.tex' file when a 'make clean' is requested.
    git-svn-id: svn://svn.code.sf.net/p/elftoolchain/code/trunk@3881 95820547-d848-0410-985e-9ae8fe0fa350

--- mk/elftoolchain.tex.mk
+++ mk/elftoolchain.tex.mk
@@ -94,6 +94,8 @@ ${DOC}.pdf:	${SRCS} ${IMAGES_MP:S/.mp$/.pdf/g} ${GENERATED_VERSION_TEX}
 	fi
 
 .if defined(GENERATED_VERSION_TEX)
+CLEANFILES+=	${GENERATED_VERSION_TEX}
+
 ${GENERATED_VERSION_TEX}:	.PHONY
 	${TOP}/libelftc/make-toolchain-version -t ${TOP} -o ${.TARGET} -h '' -p
 .endif
